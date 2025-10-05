0c68d16e06cfcbe9dbfc8e9f1f0dbf592346b20d
malte998 <soller.malte@gmail.com>
2025-10-06 00:31:28 +0200

    added title for the pages - not working

--- frontend/src/scenes/dashboard/index.jsx
+++ frontend/src/scenes/dashboard/index.jsx
@@ -1,6 +1,14 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
 
-const dashboard = () => {
-    return <div>dashboard</div>
-}
+const Dashboard = () => {
+  return (
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+      </Box>
+    </Box>
+  );
+};
 
-export default dashboard;
+export default Dashboard;
